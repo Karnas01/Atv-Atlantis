6d9a23813e3e028af5a93bebd545f6e7f3611fb6
Arthur Karnas <arthurkr1@hotmail.com>
2024-11-29 17:30:10 -0300

    AtvIII

--- src/ts/menus/menuPricipal.ts
+++ src/ts/menus/menuPricipal.ts
@@ -11,6 +11,13 @@ export default class MenuPrincipal implements Menu {
         console.log(`| 2 - Editar cliente`)
         console.log(`| 3 - Listar cliente(s)`)
         console.log(`| 4 - Excluir cliente`)
+        console.log(`----------------------`)
+        console.log(`| Opções para gestão:`)
+        console.log(`----------------------`)
+        console.log(`| 5 - Listar acomodações`)
+        console.log(`| 6 - Realizar Hospedagem`);
+        console.log(`| 7 - Listar Hóspedes`);
+        console.log(`----------------------`)
         console.log(`****************************`)
         console.log(`| 0 - Sair`)
         console.log(`----------------------`)
--- src/ts/processos/tipos/tipoListagemClientes.ts
+++ src/ts/processos/tipos/tipoListagemClientes.ts
@@ -1,15 +1,15 @@
 import Processo from "../../abstracoes/processo";
 import MenuTipoListagemClientes from "../../menus/menuTipoListagemClientes";
-import ListagemTitulares from "../listagens/listagemTitulares";
-import ListagemDependentes from "../listagens/listagemDependentes";
-import ListagemTitularDependente from "../listagens/listagemTitularParaDependente";
+import ListagemTitulares from "../listar/listagemTitulares";
+import ListagemDependentes from "../listar/listagemDependente";
+import ListagemTitularDependente from "../listar/listagemTitularParaDependente";
 
 export default class TipoListagemClientes extends Processo {
-    constructor(){
+    constructor() {
         super()
         this.menu = new MenuTipoListagemClientes()
     }
-    
+
     processar(): void {
         this.menu.mostrar()
         this.opcao = this.entrada.receberNumero('Qual a opção desejada?')
@@ -17,14 +17,16 @@ export default class TipoListagemClientes extends Processo {
             case 1:
                 this.processo = new ListagemTitulares()
                 this.processo.processar()
-                break;
+                break
             case 2:
-                this.processo =  new ListagemDependentes()
+                this.processo = new ListagemDependentes()
                 this.processo.processar()
                 break
             case 3:
                 this.processo = new ListagemTitularDependente()
                 this.processo.processar()
+            case 4:
+                this.execucao = false
                 break
             default:
                 console.log('Opção não entendida... :(')
